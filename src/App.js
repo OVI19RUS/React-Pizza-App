b8076329ec28272230f97b432b0f5d97db40db39
OVI19RUS <ovechkin19@gmail.com>
2021-12-01 09:33:53 +0300

    Adding Redux logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import axios from "axios";
+import { connect } from 'react-redux'
+
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 import { Route, Routes } from 'react-router-dom';
-import axios from "axios";
+import { setPizzas as setPizzasAction} from "./redux/actions/pizzas";
 
-function App() {
-  const [pizzas, setPizzas] = useState([])
+class App extends React.Component {
+  componentDidMount() {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => { this.props.setPizzas(data.pizzas); })
+  }
 
-  useEffect(() => { axios.get('http://localhost:3000/db.json').then(({ data }) => { setPizzas(data.pizzas) }) }, []);
-
-  return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route exact path='/' element={<Home items={pizzas} />} />
-          <Route exact path='/cart' element={<Cart />} />
-        </Routes>
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route exact path='/' element={<Home items={this.props.items} />} />
+            <Route exact path='/cart' element={<Cart />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return { 
+    items: state.pizzas.items,
+    filter: state.filters,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setPizzas: (items) => dispatch(setPizzasAction(items))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import pizzasReducer from './pizzas';
-import filterReducer from './filter'
+import filterReducer from './filters'
 
 const rootReducer = combineReducers({
     pizzas: pizzasReducer,
